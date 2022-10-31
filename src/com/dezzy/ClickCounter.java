c49d40addee6b5350878fc6c2545fada47c35d8c
Dezzy12 <desmondneba134@gmail.com>
2022-10-31 07:10:17 +0100

    feat: ClickCounter program

---
+++ src/com/dezzy/ClickCounter.java
@@ -0,0 +1,51 @@
+package com.dezzy;
+
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class ClickCounter extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    Button btn;
+    Label label;
+    int iClickCount = 0;
+
+    @Override
+    public void start(Stage primaryStage){
+        // Create button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
+
+        // Create label
+        label = new Label();
+        label.setText("You have not clicked the button.");
+
+        // Add the label and the button to the layout pane
+        BorderPane pane = new BorderPane();
+        pane.setTop(label);
+        pane.setCenter(btn);
+
+        // Add the layout pane to a scene
+        Scene scene = new Scene(pane, 250,150);
+
+        // Finalize and show stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Click Counter");
+        primaryStage.show();
+    }
+    public void buttonClick(){
+        iClickCount++;
+        if (iClickCount == 1){
+            label.setText("You have clicked once.");
+        }else{
+            label.setText("You have clicked " + iClickCount + " times.");
+        }
+    }
+
+}
